0cb8cd52fd7f959da17443dc9b0e7e17bcf39525
Laur Pulliainen <pulliainen.laur@gmail.com>
2015-09-12 19:40:35 +0300

    lopullinen versio 2

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,11 +1,13 @@
-INSERT INTO Kayttaja (nimi, salasana) VALUES ('Atte Admin', '1221');
+INSERT INTO Kayttaja (nimi, salasana) VALUES ('Atte Admin', 'atte');
 INSERT INTO Kayttaja (nimi, salasana) VALUES ('Seppo Pekka', 'seppis123');
 
 INSERT INTO Aanestys (nimi, luonut, kuvaus, paattyy, kirjautuminen) VALUES ('Vaalit', '1', 'Hienot Vaalit', '2015-12-25', 'True');
-INSERT INTO Aanestys (nimi, luonut, kuvaus, paattyy, kirjautuminen) VALUES ('Vaalit 2', '1', 'Toiset Vaalit', '2016-12-25', 'True');
+INSERT INTO Aanestys (nimi, luonut, kuvaus, paattyy, kirjautuminen) VALUES ('Vaalit 2', '1', 'Toiset Vaalit', '2016-12-25', 'False');
+INSERT INTO Aanestys (nimi, luonut, kuvaus, paattyy, kirjautuminen) VALUES ('Vaalit 3', '1', 'Vanhentunut', '2014-12-25', 'False');
 
-INSERT INTO Ehdokas (aanestys_id, nimi, aania) VALUES ('1', 'Juha Sipilä', '3');
-INSERT INTO Ehdokas (aanestys_id, nimi, aania) VALUES ('1', 'Mr Stubb', '4');
+INSERT INTO Ehdokas (aanestys_id, nimi, aania) VALUES ('1', 'Juha Sipilä', '0');
+INSERT INTO Ehdokas (aanestys_id, nimi, aania) VALUES ('1', 'Mr Stubb', '0');
 INSERT INTO Ehdokas (aanestys_id, nimi, aania) VALUES ('2', 'Jappe Diktaattori', '4000');
+INSERT INTO Ehdokas (aanestys_id, nimi, aania) VALUES ('3', 'Kylläpäs', '100');
+INSERT INTO Ehdokas (aanestys_id, nimi, aania) VALUES ('3', 'Eipäs', '101');
 
-INSERT INTO Aanestaneet (kayttaja_id, aanestys_id, ehdokas_id) VALUES ('1', '1', '2');
